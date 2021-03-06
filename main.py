0b00c39c76d9dec2c6f007599f3a4a7d837252aa
1Zarkos1 <zarkos777@gmail.com>
2021-03-06 20:40:10 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import re
+from flask import Flask, render_template, request, flash, session, redirect, make_response
+from socketio import Server, WSGIApp
+
+app = Flask(__name__, template_folder='.')
+app.config['SECRET_KEY'] = 'testKey'
+sio = Server()
+app.wsgi_app = WSGIApp(sio, app.wsgi_app)
+
+current_users = {}
+
+@sio.on('connect')
+def connect(sid, environ, auth=None):
+    p = re.compile(r'username=(.*);?')
+    username = p.search(environ.get('HTTP_COOKIE')).group(1)
+    current_users[sid] = username
+    sio.emit('system', {'sender': 'System message', 'message': f'{username} entered chat'})
+
+@sio.on('disconnect')
+def disconnect(sid):
+    username = current_users[sid]
+    del current_users[sid]
+    sio.emit('system', {'sender': 'System message', 'message': f'{username} left chat'})
+
+@sio.on('message')
+def message(sid, data):
+    sio.emit('message', {'sender': current_users[sid], 'message': data})
+
+@app.route('/', methods=['POST', 'GET'])
+def main():
+    resp = make_response(render_template('index.html'))
+    if request.method == 'POST':
+        username = request.form.get('username')
+        if not username:
+            return resp
+        elif username in current_users.values():
+            flash('That username is already taken! Choose another.')
+        else:
+            resp = make_response(redirect('/'))
+            resp.set_cookie('username', username)
+    return resp
+
+@app.route('/leave-chat')
+def leave():
+    resp = make_response(redirect('/'))
+    resp.delete_cookie('username')
+    return resp
+
+@app.context_processor
+def get_cookie():
+    user = request.cookies.get('username')
+    if user and user in current_users.values():
+        user = None
+    return dict(user=user)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
